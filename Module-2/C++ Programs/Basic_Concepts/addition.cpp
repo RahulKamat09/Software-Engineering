f3c32a2c96eabf16d2f4a431efe92ed675a2dbcb
Rahul Kamat <kamatrahul415@gmail.com>
2025-10-07 14:43:52 +0530

    first commit

---
+++ Module-2/C++ Programs/Basic_Concepts/addition.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter Value of A:";
+    cin>>a;
+    cout<<"Enter The Value of B:";
+    cin>>b;
+    cout<<"Addition of "<<a<<" and "<<b<<" is="<<a+b;
+}
---
+++ Module-2/C++ Programs/Basic_Concepts/basic.cpp
@@ -0,0 +1,9 @@
+#include<iostream> //Header File
+using namespace std;//Reference or intermediary for iostream
+int main()//Main Function
+{
+    int a;
+    /8cout<<"Enter The Number:";//Console Output
+    cin>>a;//Console input
+    cout<<a<<" Square is:"<<a*a;//Printing Square
+}
